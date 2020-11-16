37d6e2ee1438fb81edf035a92207c0318b1cf05f
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-11-16 17:52:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,55 @@
-img="";
-status="";
-objects="";
-i=0;
+
+img = "";
+objects = [];
+status = "";
+
 function preload(){
-    img=loadImage('bed_room.jpg');
+  img = loadImage('dog_cat.jpg');
 }
-function setup(){
-    canvas=createCanvas(380,380);
-    canvas.center();
-    
-    video=createCapture(VIDEO);
-    video.size(380,380);
-    video.hide();
 
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="Status : Detecting Objects";
-}
-function draw(){
 
-       image(video,0,0,380,380);
-       if(status!=""){
-        objectDetector.detect(video,gotResult);
-        r=random(255);
-        g=random(255);
-        b=random(255);
-        for(i=0;i<objects.length;i++){
-            document.getElementById("number_of_objects").innerHTML="Number of objects detected are : "+objects.length;
-               document.getElementById("status").innerHTML="Staus : Object Detected";
-               fill(r,g,b);
-               percent=floor(objects[i].confidence*100)+"%";
-               text(objects[i].label+" "+percent,objects[i].x+15,objects[i].y+15);
-               noFill();
-               stroke(r,g,b);
-               rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
-           }
-       }
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
 
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
 }
-function modelLoaded(){
-    console.log("Model Loaded!")
-    status=true;
-    objectDetector.detect(video,gotResult);
-}
+
 function gotResult(error, results) {
-    if (error) {
-      console.log(error);
-    }
-    console.log(results);
-    objects = results;
+  if (error) {
+    console.log(error);
   }
-    
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+ 
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+}
